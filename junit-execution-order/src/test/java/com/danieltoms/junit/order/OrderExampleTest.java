5f0b0013e419b64f7deea45bfa3bf8b700c7771b
Daniel Toms <dtoms@danieltoms.com>
2013-08-17 11:21:35 -0700

    add skeleton

---
+++ junit-execution-order/src/test/java/com/danieltoms/junit/order/OrderExampleTest.java
@@ -0,0 +1,14 @@
+package com.danieltoms.junit.order;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author dtoms
+ */
+public class OrderExampleTest {
+	@Test
+	public void calculateOne() {
+		Assert.fail("arg");
+	}
+}
